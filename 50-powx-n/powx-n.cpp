81838ca07235942bcf0c96b0c35e6ac761f79db5
pradeepguptha <pradeepguptha.18@gmail.com>
2026-07-16 23:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.60%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long exp = n ;
+        if(exp < 0 ){
+            exp = - exp;
+            x = 1/x;
+        }
+        double ans= 1.0;
+        while ( exp > 0 ){
+            if(exp%2==1){
+                ans*=x;
+                }        
+            x*=x;
+            exp/=2;
+    }
+    return ans;
+    }
+    
+    };
